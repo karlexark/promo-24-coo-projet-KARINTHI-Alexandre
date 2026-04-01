c51c2bf5224826b224e1addbbfcdae721fada449
A.KARINTHI <alexandre.karinthi@gmail.com>
2026-04-01 09:36:15 +0200

    FEAT MiniDuck.java

--- src/duckcorp/duck/MiniDuck.java
+++ src/duckcorp/duck/MiniDuck.java
@@ -8,8 +8,23 @@ package duckcorp.duck;
  *   - Implémentez le constructeur MiniDuck(int qualityScore) avec un appel à super
  *   - Implémentez getBasePrice() et describe()
  * @author Roussille Philippe <roussille@3il.fr>
+ * @author Karinthi Alexandre <karintha@3il.fr>
  */
-public class MiniDuck {
+public class MiniDuck extends Duck {
 
     public static final double BASE_PRICE = 12.0;
+
+    public MiniDuck(int qualityScore) {
+        super(DuckType.MINI, qualityScore);
+    }
+
+    @Override
+    public double getBasePrice() {
+        return BASE_PRICE;
+    }
+
+    @Override
+    public String describe() {
+        return "Mini Canard";
+    }
 }
