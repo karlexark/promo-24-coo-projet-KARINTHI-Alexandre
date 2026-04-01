b662699bee01330d2a5e7d938071431f58bf1fab
A.KARINTHI <alexandre.karinthi@gmail.com>
2026-04-01 09:32:25 +0200

    FEAT StandardDuck.java

--- src/duckcorp/duck/StandardDuck.java
+++ src/duckcorp/duck/StandardDuck.java
@@ -8,8 +8,23 @@ package duckcorp.duck;
  *   - Implémentez le constructeur StandardDuck(int qualityScore) avec un appel à super
  *   - Implémentez getBasePrice() et describe()
  * @author Roussille Philippe <roussille@3il.fr>
+ * @author Karinthi Alexandre <karintha@3il.fr>
  */
-public class StandardDuck {
+public class StandardDuck extends Duck {
 
     public static final double BASE_PRICE = 25.0;
+
+    public StandardDuck(int qualityScore) {
+        super(DuckType.STANDARD, qualityScore);
+    }
+
+    @Override
+    public double getBasePrice() {
+        return BASE_PRICE;
+    }
+
+    @Override
+    public String describe() {
+        return "Canard standard";
+    }
 }
